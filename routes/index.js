3a3769c8c753531cb35a79532bc884b01970d552
Benson.YBS <yehbo0921@gmail.com>
2022-10-25 22:55:09 +0800

    fix: modify route's order

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const home = require('./modules/home.js')
 const short_urls = require('./modules/short_urls.js')
 //Set router
-router.use('/', home)
 router.use('/short_urls', short_urls)
+router.use('/', home)
 
 module.exports = router
